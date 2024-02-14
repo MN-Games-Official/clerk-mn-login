111e90ea8289bc58d1d1cf62fbbaccbea499aee9
MN-Games-Official <160078250+MN-Games-Official@users.noreply.github.com>
2024-02-14 17:50:10 -0600

    Update Footer.tsx

--- src/pages/components/Footer.tsx
+++ src/pages/components/Footer.tsx
@@ -5,27 +5,27 @@ export default function Footer() {
   return (
     <footer className="flex items-center h-20 gap-1 px-8 font-medium border-t md:px-20">
       <Image
-        src="/clerk.svg"
-        alt="Clerk Logo"
+        src=""
+        alt="MN Games"
         width={64}
         height={32}
         priority
       />
-      <span className="text-sm">© 2023</span>
+      <span className="text-sm">MN Games™️ 2024</span>
       <nav className="flex justify-end grow sm:gap-2">
         <a
           className="flex gap-2 px-3 py-2 text-sm font-semibold text-gray-600 transition duration-100 rounded-md hover:text-gray-800"
-          href="https://clerk.com/docs?utm_source=vercel-template&utm_medium=template_repos&utm_campaign=nextjs_template"
+          href=""
         >
           <div className="m-auto">
             <Docs />
           </div>
-          <span className="hidden sm:inline"> Visit Clerk Docs</span>
-          <span className="inline sm:hidden"> Docs</span>
+          <span className="hidden sm:inline"></span>
+          <span className="inline sm:hidden"></span>
         </a>
         <a
           className="flex gap-2 px-3 py-2 text-sm font-semibold text-gray-600 transition duration-100 rounded-md hover:text-gray-800"
-          href="https://github.com/devagrawal09/clerk-nextjs-template"
+          href="https://together-ghoul-7.accounts.dev/sign-up"
         >
           <div className="m-auto">
             <Github />
