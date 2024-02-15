39c9b3d4c8d80d94a79f051c975fb546ae0105c8
MN-Games-Official <160078250+MN-Games-Official@users.noreply.github.com>
2024-02-14 21:40:00 -0600

    Update Main.tsx

--- src/pages/components/Main.tsx
+++ src/pages/components/Main.tsx
@@ -51,18 +51,6 @@ Welcome we are so glad you could join us!
             href=""
             className="font-medium text-primary-600 hover:underline"
           >
-         
-<script type="text/javascript">
-var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
-(function(){
-var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
-s1.async=true;
-s1.src='https://embed.tawk.to/64879be5cc26a871b0222039/1hkpk6gll';
-s1.charset='UTF-8';
-s1.setAttribute('crossorigin','*');
-s0.parentNode.insertBefore(s1,s0);
-})();
-</script>
 
             
           </a>
