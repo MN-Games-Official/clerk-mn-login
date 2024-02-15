1f0efb3456fa3c13066f6764d96836f4faf756a2
MN-Games-Official <160078250+MN-Games-Official@users.noreply.github.com>
2024-02-15 10:29:38 -0600

    Update Main.tsx

--- src/pages/components/Main.tsx
+++ src/pages/components/Main.tsx
@@ -18,20 +18,18 @@ export default function Main() {
             Welcome to MN Games!
           </p>
           <div className="flex gap-2 mt-8">
-            <button
-              className="flex content-center gap-2 px-4 py-2 font-semibold text-white transition-colors duration-200 rounded-lg bg-primary-600 hover:bg-primary-700"
-              onClick={() => window.location.reload()}
-            >
-              <SignUp />
-              <span>Sign Up</span>
-            </button>
-            <button
-              className="flex content-center gap-2 px-4 py-2 font-semibold text-white transition-colors duration-200 rounded-lg bg-primary-600 hover:bg-primary-700"
-              onClick={() => window.location.reload()}
-            >
-              <SignIn />
-              <span>Sign In</span>
-            </button>
+            <Link href="/sign-up">
+              <a className="flex content-center gap-2 px-4 py-2 font-semibold text-white transition-colors duration-200 rounded-lg bg-primary-600 hover:bg-primary-700">
+                <SignUp />
+                <span>Sign Up</span>
+              </a>
+            </Link>
+            <Link href="/login">
+              <a className="flex content-center gap-2 px-4 py-2 font-semibold text-white transition-colors duration-200 rounded-lg bg-primary-600 hover:bg-primary-700">
+                <SignIn />
+                <span>Sign In</span>
+              </a>
+            </Link>
           </div>
         </div>
         <div className="flex flex-col justify-center">
