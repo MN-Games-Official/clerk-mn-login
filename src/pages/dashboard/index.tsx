0fb585b0e43d891feb824361080fc0f415d1654c
MN-Games-Official <160078250+MN-Games-Official@users.noreply.github.com>
2024-02-16 10:06:59 -0600

    Update index.tsx

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -34,7 +34,7 @@ export default function DashboardPage() {
         {user && (
           <>
             <h1 className="text-3xl font-semibold text-black">
-               Hi, {user.firstName || "Stranger"} Please scroll to the buttons below your user info to play games!
+               Hi, {user.firstName || "Stranger"}
             </h1>
             <div className="grid-container">
               <button className="button-centered" onClick={() => {
